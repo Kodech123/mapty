fa9fb8bcc281455a442d9e549160d0498df5cbb6
Kodec felix <felixchukwudi126@gmail.com>
2025-10-15 01:27:42 +0100

    Mapping architecture

---
+++ Mapty app/script.js
@@ -0,0 +1,145 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+class Workout {
+  date = new Date();
+  id = new Date().getTime().toString().slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcPace();
+  }
+  calcPace() {
+    // Formula for calculating Pace is : Time / Distance
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    //The formula for speed is: Distance/ Time
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycle1 = new Cycling([39, -12], 27, 95, 523);
+console.log(run1, cycle1);
+
+/////////////////////////////////////
+// THE APPLICATION ARCHITECTURE
+class App {
+  #map;
+  #mapE;
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField.bind(this));
+  }
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
+        }
+      );
+    }
+  }
+
+  _loadMap(position) {
+    let { latitude } = position.coords;
+    let { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+     const coords = [latitude, longitude];
+
+    this.#map = L.map('map').setView(coords, 13);
+
+    L.tileLayer(
+      'https://tile.jawg.io/jawg-streets/{z}/{x}/{y}{r}.png?access-token=iw2zHLa099nToloJFH00IDmrnMynUA0PoX25JnyPt3dnt099uouHEO5OMj5EBzuc',
+      {
+        attribution: '&copy; OpenStreetMap, © JawgMaps',
+      }
+    ).addTo(this.#map);
+
+    L.marker(coords).addTo(this.#map).bindPopup('Present Location').openPopup();
+
+    //handling click on map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+  _showForm(mapEvent) {
+    console.log(mapEvent);
+
+    this.#mapE = mapEvent;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+  _toggleElevationField() {
+    //toggleElevationField()
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+  _newWorkout(e) {
+      // const distance = +inputDistance.value
+      // const duration = +inputDuration.value
+      // const cadence = +inputCadence.value
+
+      // Running(this, this.#coords, distance, duration, cadence)
+
+    //newWorkout()
+    e.preventDefault();
+    const { lat, lng } = this.#mapE.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxwidth: 250,
+          autoClose: false,
+          closeOnClick: false,
+          minWidth: 100,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+
+      
+
+      //clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+  }
+}
+
+const app = new App();
+// const run1 = new Running()
+// console.log(run1);
+
